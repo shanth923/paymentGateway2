791e5a752f40ea0c15f690fdfe09fbaea0535141
shanth kuamer <shanth923@gmail.com>
2020-01-20 13:46:19 +0530

    Initial Commit

---
+++ paymentGateway2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  paymentGateway2
+//
+//  Created by R Shantha Kumar on 1/20/20.
+//  Copyright © 2020 R Shantha Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
